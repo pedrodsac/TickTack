c15207c98b1c7ebdea3e175c7918cec0f09b4e82
pedrodsac <pedrodsac@gmail.com>
2023-11-05 17:26:25 +0100

    Initial Commit

---
+++ Tick Tack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tick Tack
+//
+//  Created by Pedro Cordeiro on 05/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tick Tack/Tick_TackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tick_TackApp.swift
+//  Tick Tack
+//
+//  Created by Pedro Cordeiro on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tick_TackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
